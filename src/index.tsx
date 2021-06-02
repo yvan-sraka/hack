c4b427a848ec9fc7002b61a01248c9b871ea3bc2
Nicolas Hovart <hovart.nicolas@gmail.com>
2021-06-02 23:02:18 +0200

    Create Component Button

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import { useState } from 'react'
 import * as ReactDOM from 'react-dom'
-import { ProgressBar, List } from './components'
+import { ProgressBar, List, Button } from './components'
 
 const ProgressBarSample = () => {
   let [percent, setPercent] = useState(0)
@@ -53,10 +53,46 @@ const ProgressBarSampleError = () => {
 
 const mountNode = document.getElementById('app')
 ReactDOM.render(
-  <div>
+  <div className="container">
     <ProgressBarSample /><br />
     <ProgressBarSampleWithPercents /><br />
     <ProgressBarSampleSuccess /><br />
     <ProgressBarSampleError /><br />
     <List data={[{ value: 'shadock language', next: ['ga', 'bu', 'zo', 'meu'] }, 'one', 'two', 'three']} />
+    <br />
+    <h2> Buttons</h2>
+    <p>
+      <Button />
+      <Button type="primary"/>
+      <Button type="success"/>
+      <Button type="info"/>
+      <Button type="warning"/>
+      <Button type="error"/>
+    </p>
+    <p>
+      <Button type="btn-ghost" />
+      <Button type="primary-ghost" />
+      <Button type="success-ghost" />
+      <Button type="info-ghost" />
+      <Button type="warning-ghost" />
+      <Button type="error-ghost" />
+    </p>
+    <p>
+      <Button type="block" />
+    </p>
+    {/* TODO: add code */}
+    {/* <pre>
+      <code  className="lang-html">
+        &lt;button className=&quot;btn btn-default&quot;/&gt;
+        &lt;button className=&quot;btn btn-primary&quot;/&gt;
+        &lt;button className=&quot;btn btn-success&quot;/&gt;
+        &lt;button className=&quot;btn btn-info&quot;/&gt;
+        &lt;button className=&quot;btn btn-warning&quot;/&gt;
+        &lt;button className=&quot;btn btn-error&quot;/&gt;
+
+        &lt;button className=&quot;btn btn-primary btn-ghost&quot;&gt;Ghost Button/&gt;
+
+        &lt;button className=&quot;btn btn-primary btn-block&quot;&gt;Block Level Button/&gt;
+      </code>
+    </pre> */}
   </div>, mountNode)
