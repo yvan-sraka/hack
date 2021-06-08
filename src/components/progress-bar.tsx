d0f6450a997fccf903f8cf053017cb11f95ffadc
Adrien Zinger <azinger@alpenite.com>
2021-06-08 09:34:03 +0200

    [Fix] Broken build react with parcel

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -3,17 +3,17 @@
 import React from 'react'
 import '../css/rstrtt.css'
 
-const ProgressBar = (props: { showPercent: string | number; success: string | number; percent: string }) => {
+const ProgressBar = (props: { showPercent?: string | number, success?: string | number | boolean, percent: number }) => {
   const classname = {
     undefined: 'progress-bar',
     false: 'progress-bar',
     true: 'progress-bar-show-percent'
-  }[props.showPercent]
+  }[props.showPercent !== undefined ? props.showPercent : 'undefined']
   const classbarfilled = {
     undefined: 'progress-bar-filled',
     false: 'progress-bar-filled-error',
     true: 'progress-bar-filled-success'
-  }[props.success]
+  }[props.success !== undefined ? props.success.toString() : 'undefined']
   const style = { width: props.percent + '%' }
   const dataFilled = `Loading ${props.percent}%`
   return (<div className={classname}><div className={classbarfilled} style={style} data-filled={dataFilled} /></div>)
